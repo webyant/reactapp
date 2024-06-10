1426b5df4e545f0ef3a6c6505d0adddffb14db93
Webyant <contact@webyant.com>
2024-06-10 17:00:16 +0530

    Update HomePage.js

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,30 +1,7 @@
 // HomePage.js
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from "react";
 
 const HomePage = () => {
-  const [users, setUsers] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    const fetchData = async () => {
-        try {
-          const response = await axios.get('https://reqres.in/api/users/');
-          setUsers(response.data.data); // Assuming the data is nested under 'data'
-          setIsLoading(false);
-        } catch (err) {
-          setError(err.message);
-          setIsLoading(false);
-        }
-      };
-
-    fetchData();
-  }, []);
-
-  if (isLoading) return <div>Loading...</div>;
-  if (error) return <div>Error: {error}</div>;
-
   return (
     <div>
       <h1>Homepage</h1>
